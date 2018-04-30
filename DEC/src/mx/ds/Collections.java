fc7acf4dd1fdf83016324e1f1438027dbe377e9a
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-29 23:50:19 -0500

    Agrega ejemplo de Hashmap y Treeset

--- DEC/src/mx/ds/Collections.java
+++ DEC/src/mx/ds/Collections.java
@@ -1,6 +1,12 @@
 package mx.ds;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeSet;
 
 public class Collections {
 
@@ -29,6 +35,48 @@ public class Collections {
 			System.out.println("Empleados: " + employee.age);
 		}
 		
+		// Linked List 
+		// Need to read/write data
+		LinkedList<String> ls = new LinkedList<>();
+		ls.add("Jose");
+		ls.add("Roberto");
+		ls.add("Almaraz");
+				
+		Iterator<String> itr = ls.iterator();
+		while(itr.hasNext()) {
+			System.out.println("Valores siguientes: " + itr.next());
+		}
+		
+		// HashMap 
+		HashMap<Integer, Employee> mapp = new HashMap<>();
+		mapp.put(1, new Employee("Jose", 11));
+		mapp.put(2, new Employee("Jose", 11));
+		mapp.put(3, new Employee("Jose", 11));
+		
+		System.out.println("Valor 0: " + mapp.get(1).name);
+		
+		for(Map.Entry m: mapp.entrySet()) {
+			System.out.println("Imprime datos: "+ ((Employee)m.getValue()).name);
+		}
+		
+		//Treeset/Hashset
+		//HashSet. Conjunto que no permite y no imprime valores repetidos
+		//como,por ejemplo, tener dos cadenas con el nombre "hola"
+		//HashSet<String> hs = new HashSet<>();
+		
+		// TreeSet. Realiza la misma funcion que HashSet con la diferencia de 
+		// que este realiza el ordenamiento de los valores ingresados dentro 
+		// de la estructura de datos. 
+		TreeSet<String> hs = new TreeSet<>();
+		hs.add("Hola");
+		hs.add("Alo");
+		hs.add("Alo");
+		hs.add("Javier");
+		
+		Iterator<String> itrs = hs.iterator();
+		while(itrs.hasNext()) {
+			System.out.println("Salida Arbol: " + itrs.next());
+		}
 	}
 	
 	static class Employee {
@@ -40,5 +88,6 @@ public class Collections {
 			this.age = age;
 		}
 	}
-
+	
+	
 }
